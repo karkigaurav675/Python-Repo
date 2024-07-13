ffabe20ada2f4f12d18a91a5191397ac6a360e60
karkigaurav675 <karkigaurav675@gmail.com>
2024-07-13 17:45:18 +0530

    practice 4 complete

---
+++ Week 1/practice4.py
@@ -0,0 +1,14 @@
+continents = ("Europe", "Africa", "Asia", "Australia", "North America", "South America")
+
+print("Tuple of continents:")
+print(continents)
+
+print("\nThird continent in the tuple")
+print(continents[2])
+
+# print("\nChanging the second continent:")
+# continents[1]="Antarctica"
+# print(continents)
+
+# error occurred "tuple" does not support item assignment as tuples are immutable i.e can't be changed
+# if we want to change data stored we should use lists instead.
