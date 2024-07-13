5eba06ba5d8f2b788890b2edc44e4fddec0ec65c
karkigaurav675 <karkigaurav675@gmail.com>
2024-07-13 17:54:58 +0530

    practice 5 complete

---
+++ Week 1/practice5.py
@@ -0,0 +1,10 @@
+newtuple = ("This is Gaurav", 25, 3.14)
+
+print("Tuple with string, integer, and float:")
+print(newtuple)
+
+gaurav, number, decimal = newtuple
+print("\ntuple into 3 variables and printing said variable:")
+print(gaurav)
+print(number)
+print(decimal)
