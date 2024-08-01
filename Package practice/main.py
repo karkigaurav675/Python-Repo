8167b0663598d1ae662ba5c3f7f42d8fcbd430d9
karkigaurav675 <karkigaurav675@gmail.com>
2024-08-01 10:10:29 +0530

    cleaned up folder structure

--- Package practice/main.py
+++ Package practice/main.py
@@ -1,7 +1,7 @@
 from My_Package.main_package.sub_package.submodule1 import function1
 from My_Package.main_package.sub_package.submodule2 import function2
-from My_Package.module1 import mod1
-from My_Package.module2 import mod2
+from My_Package.main_package.module1 import mod1
+from My_Package.main_package.module2 import mod2
 function1()
 function2()
 mod1()
